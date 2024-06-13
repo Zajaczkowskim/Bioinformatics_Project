a556ec8fdc9d5e83a68d5c6e260348b3fb8f4cb4
Filip Szlingiert <125889993+FylypO@users.noreply.github.com>
2024-06-13 21:13:55 +0200

    fresh'n'clean

--- notebooks/data_processing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def scale_bead_chain(og_middle_points, nr_connections, new_min=1, new_max=200):
-    trimmed_df = og_middle_points.head(nr_connections)
-    original_min = np.min(trimmed_df)
-    original_max = np.max(trimmed_df)
-    df_scaled = new_min + ((trimmed_df - original_min) * (new_max - new_min) / (original_max - original_min))
-    
-    return df_scaled.astype('int64')
--- test_test/tester.py
+++
@@ -1,7 +0,0 @@
-from model import Model
-
-model = Model(500,20)
-model.run_simulation()
-
-print("Expected loops: ")
-print(model.expected_loops)
